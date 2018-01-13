6da20bdb96f6b5ffb2dca0b33c52dd01c035dbcc
Parth Thakur <parththakur@gmail.com>
2018-01-13 12:37:05 +0530

    --

--- Astronomy_Picture.py
+++ Astronomy_Picture.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 import requests
 import re
 import io
@@ -13,8 +14,12 @@ save_path = r"C:\Users\parth\Pictures\APOD"
 def get_date():
     try:
         date = input('Enter Date for the Astronomy picture you want to see. [dd/mm/yy] ("T" for today.) \n').split('/')
-        if date[0].lower() in ['', 't']:
+        if date[0].lower()[0] in ['t']:
             date = datetime.datetime.now().strftime("%d-%m-%y").split('-')
+        if date[0].lower()[0] in ['r']:
+            date = [random.randint(1, 28), random.randint(1, 12), random.choice([random.randint(0, 17), random.randint(95, 99)])]
+            print('random : ')
+            print(date)
         date = list(map(int, date))
         x = str(datetime.datetime(year=date[2], month=date[1], day=date[0])).split('-')
         y1 = [x[0][2:], x[1], x[2][:2]]
@@ -29,15 +34,14 @@ def get_date():
 def get_explanation():
     raw = soup.findAll('p')[2]
     pos = [m.start() for m in re.finditer('<p>', str(raw))]
-    raw = str(raw)[:pos[1]]
+    raw = str(raw)[:pos[1-len(pos)]]
     soup_ = BeautifulSoup(raw, 'html.parser')
     raw = str(soup_.findAll('p')[0].text)
     return (raw.replace('\n', ' ')).replace('  ', ' ')
 
 
 def get_image():
-    p = image_date.ctime()
-    print("Astronomy Picture of the Day for " + p[4:-13] + "'" + p[-2:] + " is titled:")
+    print("Astronomy Picture of the Day for " + d + " is titled:")
     print(title)
 
     picture_path = soup.findAll('a')[1]
@@ -58,14 +62,18 @@ def download_image():
     image_raw = requests.get(image_links['full-res'])
     image_bytes = io.BytesIO(image_raw.content)
     image = Image.open(image_bytes).convert("RGB")
-    image.save(save_path + '\\' + title.replace(':', '') + '.png', 'PNG')
+    image.save(save_path + '\\' + title.replace(':', '') + d + '.png', 'PNG')
 
 
 image_date, astro_date = get_date()
+
+p = image_date.ctime()
+d = p[4:-13] + "'" + p[-2:]
+
 image_URL = astro_URL.replace('[%%]', astro_date)
 response = requests.get(image_URL)
 if response.status_code != 200:
-    print('The date entered is out of range. Please enter a date between January 1, 1995 and today.')
+    print('The date (' + d + ') entered is out of range. Please enter a date between June 16, 1995 and today.')
     exit('Invalid Date')
 soup = BeautifulSoup(response.text, 'html.parser')
 
